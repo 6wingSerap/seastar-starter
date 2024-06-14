f10ea0b08793a2b5eaa331ebd62ba9b7f8b973cf
6wingSerap <vladimir.luniachenko@gmail.com>
2024-06-14 14:23:41 +0300

    all CRUDs seems working

--- INCLUDE/DB/MongoInterface.h
+++ INCLUDE/DB/MongoInterface.h
@@ -14,38 +14,38 @@
 class MongoDBInterface
 {
     public:
-    static seastar::future<> createDB(const sstring& db_name)
+    static seastar::future<> createDB(const string_view db_name)
     {
         TRY;
 
-        mongocxx::database db = (co_await db_clients())[db_name.c_str()];
+        mongocxx::database db = (co_await db_clients())[db_name];
         db.create_collection("DummyCollection"sv);
 
         CATCH{ LG_ERROR("Failed to create database: {} exception: {}", db_name, std::current_exception()); }
     }
 
-    static seastar::future<> createCollection(const sstring& db_name, const sstring& coll_name, const document::view_or_value& options = {})
+    static seastar::future<> createCollection(const string_view db_name, const string_view coll_name, const document::view_or_value& options = {})
     {
         TRY;
 
-        mongocxx::database db = (co_await db_clients())[db_name.c_str()];
-        db.create_collection(coll_name.c_str(), options);
+        mongocxx::database db = (co_await db_clients())[db_name];
+        db.create_collection(coll_name, options);
 
         CATCH{ LG_ERROR("Failed to create collection: {} exception: {}", coll_name, std::current_exception()); }
     }
 
-    static seastar::future<> createDocument(const sstring& db_name, const sstring& coll_name, const document::view_or_value& document = {})
+    static seastar::future<> createDocument(const string_view db_name, const string_view coll_name, const document::view_or_value& document = {})
     {
         TRY;
 
-        mongocxx::database db = (co_await db_clients())[db_name.c_str()];
-        mongocxx::collection coll = db[coll_name.c_str()];
+        mongocxx::database db = (co_await db_clients())[db_name];
+        mongocxx::collection coll = db[coll_name];
         coll.insert_one(document);
 
         CATCH{ LG_ERROR("Failed to create document: {} exception: {}", coll_name, std::current_exception()); }
     }
 
-    static seastar::future<seastar::lw_shared_ptr<document::view_or_value>> readDB(const sstring& dbName, const bool list_all)
+    static seastar::future<seastar::lw_shared_ptr<document::view_or_value>> readDB(const string_view dbName, const bool list_all)
     {
         TRY;
 
@@ -90,11 +90,11 @@ class MongoDBInterface
         co_return nullptr;
     }
 
-    static seastar::future<seastar::lw_shared_ptr<document::view_or_value>> readCollection(const sstring& dbName, const sstring& collectionName, const bool listAll)
+    static seastar::future<seastar::lw_shared_ptr<document::view_or_value>> readCollection(const string_view dbName, const string_view collectionName, const bool listAll)
     {
         TRY;
 
-        auto db = (co_await db_clients())[dbName.c_str()];
+        auto db = (co_await db_clients())[dbName];
         auto listedCollections = db.list_collections();
 
         bsoncxx::builder::basic::array arrayBuilder;
@@ -146,16 +146,21 @@ class MongoDBInterface
         co_return nullptr;
     }
 
-    static seastar::future<seastar::lw_shared_ptr<document::view_or_value>> readDocument(const sstring& dbName, const sstring& collectionName, const document::view_or_value& filter = {}, const document::view_or_value& options = {})
+    static seastar::future<seastar::lw_shared_ptr<document::view_or_value>> readDocument(const string_view dbName, const string_view collectionName, const document::view_or_value& filter = {}, const document::view_or_value& options = {})
     {
         TRY;
 
-        auto db = (co_await db_clients())[dbName.c_str()];
-        auto collection = db[collectionName.c_str()];
+        auto db = (co_await db_clients())[dbName];
+        auto collection = db[collectionName];
         mongocxx::options::find opts{};
-        opts.allow_disk_use(options.view()["allow_disk_use"].get_int32().value == 1);
-        opts.batch_size(options.view()["batch_size"].get_int32().value);
-        opts.limit(options.view()["limit"].get_int32().value);
+
+        if (options.view()["allow_disk_use"]) { opts.allow_disk_use(options.view()["allow_disk_use"].get_int32().value == 1); }
+
+        if (options.view()["batch_size"]) { opts.batch_size(options.view()["batch_size"].get_int32().value); }
+
+        if (options.view()["limit"]) { opts.limit(options.view()["limit"].get_int32().value); }
+        // opts.hint()
+        // TODO: больше настроек
 
         auto doc_cursor = collection.find(filter, opts);
 
@@ -174,79 +179,100 @@ class MongoDBInterface
         co_return nullptr;
     }
 
-    static seastar::future<> updateCollection(const sstring& dbName, const sstring& oldName, const sstring& newName)
+    static seastar::future<> updateCollection(const string_view dbName, const string_view oldName, const string_view newName)
     {
         TRY;
 
         auto& client = co_await db_clients();
-        auto db = (co_await db_clients())[dbName.c_str()];
-        db[oldName.c_str()].rename(newName.c_str());
+        auto db = (co_await db_clients())[dbName];
+        db[oldName].rename(newName);
 
         CATCH{ LG_ERROR("Failed to update collection: {} exception: {}", oldName, std::current_exception()); }
     }
 
-    static seastar::future<> updateDocument(const sstring& dbName, const sstring& collectionName, bool updmany = false, const document::view_or_value& filter = {}, const document::view_or_value& update = {})
+    static seastar::future<> updateDocument(const string_view dbName, const string_view collectionName, bool updmany = false, const document::view_or_value& filter = {}, const document::view_or_value& update = {})
     {
         TRY;
 
-        auto db = (co_await db_clients())[dbName.c_str()];
-        auto collection = db[collectionName.c_str()];
+        auto db = (co_await db_clients())[dbName];
+        auto collection = db[collectionName];
 
-        if (updmany) { collection.update_many(filter, update); }
-        else { collection.update_one(filter, update); }
+        if (!filter.view().empty() && !update.view().empty())
+        {
+            if (updmany)
+            {
+                collection.update_many(filter, update);
+            }
+            else
+            {
+                collection.update_one(filter, update);
+            }
+        }
 
         CATCH{ LG_ERROR("Failed to update document: {} exception: {}", collectionName, std::current_exception()); }
     }
 
-    static seastar::future<> deleteDB(const sstring& dbName)
+    static seastar::future<> deleteDB(const string_view dbName)
     {
         TRY;
 
-        (co_await db_clients())[dbName.c_str()].drop();
+        (co_await db_clients())[dbName].drop();
 
         CATCH{ LG_ERROR("Failed to delete database: {} exception: {}", dbName, std::current_exception()); }
     }
 
-    static seastar::future<> deleteCollection(const sstring& dbName, const sstring& collectionName)
+    static seastar::future<> deleteCollection(const string_view dbName, const string_view collectionName)
     {
         TRY;
 
-        (co_await db_clients())[dbName.c_str()][collectionName.c_str()].drop();
+        (co_await db_clients())[dbName][collectionName].drop();
 
         CATCH{ LG_ERROR("Failed to delete collection: {} exception: {}", collectionName, std::current_exception()); }
     }
 
-    static seastar::future<> deleteDocument(const sstring& dbName, const sstring& collectionName, bool delmany = false, const document::view_or_value& filter = {})
+    static seastar::future<> deleteDocument(const string_view dbName, const string_view collectionName, bool delmany = false, const document::view_or_value& filter = {})
     {
         TRY;
 
-        auto db = (co_await db_clients())[dbName.c_str()];
-        auto collection = db[collectionName.c_str()];
+        auto db = (co_await db_clients())[dbName];
+        auto collection = db[collectionName];
 
-        if (delmany) { collection.delete_many(filter); }
-        else { collection.delete_one(filter); }
+        if (!filter.view().empty())
+        {
+            if (delmany)
+            {
+                collection.delete_many(filter);
+            }
+            else
+            {
+                collection.delete_one(filter);
+            }
+        }
 
         CATCH{ LG_ERROR("Failed to delete document: {} exception: {}", collectionName, std::current_exception()); }
     }
 
-    static seastar::future<> createIndex(const sstring& dbName, const sstring& collectionName, const document::view_or_value& keys, const document::view_or_value& index_options = {})
+    static seastar::future<> createIndex(const string_view dbName, const string_view collectionName, const document::view_or_value& keys, const document::view_or_value& index_options = {})
     {
         TRY;
 
-        auto db = (co_await db_clients())[dbName.c_str()];
-        auto collection = db[collectionName.c_str()];
+        auto db = (co_await db_clients())[dbName];
+        auto collection = db[collectionName];
 
-        collection.create_index(keys, index_options);
+        if (!keys.view().empty())
+        {
+            collection.create_index(keys, index_options);
+        }
 
         CATCH{ LG_ERROR("Failed to create index: {} exception: {}", collectionName, std::current_exception()); }
     }
 
-    static seastar::future<seastar::lw_shared_ptr<document::view_or_value>> readIndex(const sstring& dbName, const sstring& collectionName, const sstring& indexName = "")
+    static seastar::future<seastar::lw_shared_ptr<document::view_or_value>> readIndex(const string_view dbName, const string_view collectionName, const string_view indexName = "")
     {
         TRY;
 
-        auto db = (co_await db_clients())[dbName.c_str()];
-        auto collection = db[collectionName.c_str()];
+        auto db = (co_await db_clients())[dbName];
+        auto collection = db[collectionName];
         auto indexes_cursor = collection.list_indexes(); // https://www.mongodb.com/docs/manual/reference/command/listIndexes/
 
         bsoncxx::builder::basic::array arrayBuilder;
@@ -279,12 +305,12 @@ class MongoDBInterface
         co_return nullptr;
     }
 
-    static seastar::future<> deleteIndex(const sstring& dbName, const sstring& collectionName, const sstring& indexName)
+    static seastar::future<> deleteIndex(const string_view dbName, const string_view collectionName, const string_view indexName)
     {
         TRY;
 
-        auto db = (co_await db_clients())[dbName.c_str()];
-        auto collection = db[collectionName.c_str()];
+        auto db = (co_await db_clients())[dbName];
+        auto collection = db[collectionName];
 
         collection.indexes().drop_one(indexName);
 
--- main.cc
+++ main.cc
@@ -20,9 +20,14 @@
 #include <seastar/core/print.hh>
 #include <seastar/net/inet_address.hh>
 #include <seastar/util/defer.hh>
+#include "seastar/http/common.hh"
+
+#include <bsoncxx/json.hpp>
+
 #include "stop_signal.hh"
 
 #include <memory>
+#include <string_view>
 // #include <google/protobuf/
 
 #include "GLOBALS.h"
@@ -36,66 +41,271 @@ using namespace seastar::httpd;
 // sudo /home/6wingSeraph/SIXW_SS_NEXUS/seastar/scripts/seastar-addr2line -e /home/6wingSeraph/SIXW_SS_NEXUS/build/NEXUS
 // sudo mongod --dbpath /var/lib/mongo --port 27017 --bind_ip 0.0.0.0
 
+enum class mongo_class
+{
+    db,
+    collection,
+    document,
+    index
+};
+
+enum class crud_operation
+{
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE
+};
+
+crud_operation get_operation_type(const sstring& operation_str)
+{
+    if (operation_str == "create")
+    {
+        return crud_operation::CREATE;
+    }
+    else if (operation_str == "read")
+    {
+        return crud_operation::READ;
+    }
+    else if (operation_str == "update")
+    {
+        return crud_operation::UPDATE;
+    }
+    else if (operation_str == "delete")
+    {
+        return crud_operation::DELETE;
+    }
+    else
+    {
+        throw std::runtime_error("Invalid operation type: " + operation_str);
+    }
+}
+
+mongo_class get_mongo_class(const sstring& mongo_class_str)
+{
+    if (mongo_class_str == "db")
+    {
+        return mongo_class::db;
+    }
+    else if (mongo_class_str == "collection")
+    {
+        return mongo_class::collection;
+    }
+    else if (mongo_class_str == "document")
+    {
+        return mongo_class::document;
+    }
+    else if (mongo_class_str == "index")
+    {
+        return mongo_class::index;
+    }
+    else
+    {
+        throw std::runtime_error("Invalid mongo class type: " + mongo_class_str);
+    }
+}
+
 class crud_handler : public httpd::handler_base
 {
     public:
     virtual future<std::unique_ptr<http::reply>> handle(const sstring& path, std::unique_ptr<http::request> req, std::unique_ptr<http::reply> rep) override
     {
-        if (req->_method == "POST")
+        TRY;
+
+        if (req->_method != "POST")
         {
-            auto uid = req->get_query_param("uid");
-            auto operation = req->get_query_param("operation");
-            auto mongo_class = req->get_query_param("class");
-
-            // Выполнение CRUD операции в зависимости от параметров
-            if (operation == "create")
-            {
-                if (mongo_class == "db")
-                {
-                    // Создание базы данных
-                    LG.info("Creating database {}", uid);
-                }
-                else if (mongo_class == "collection")
-                {
-                    // Создание коллекции
-                    LG.info("Creating collection {}", uid);
-                }
-                else if (mongo_class == "document")
-                {
-                    // Создание документа
-                    LG.info("Creating document {}", uid);
-                }
-                else if (mongo_class == "index")
-                {
-                    // Создание индекса
-                    LG.info("Creating index {}", uid);
-                }
-            }
-            else if (operation == "read")
-            {
-                // Аналогично для операции чтения
-                LG.info("Reading {}", uid);
-            }
-            else if (operation == "update")
-            {
-                // Аналогично для операции обновления
-                LG.info("Updating {}", uid);
-            }
-            else if (operation == "delete")
-            {
-                // Аналогично для операции удаления
-                LG.info("Deleting {}", uid);
-            }
-
-            // Формирование ответа в формате JSON
-            // rep->_content = json::stream_object();
-            // Добавление необходимых полей в ответ
-
+            rep->set_status(http::reply::status_type::bad_request);
             rep->done("json");
+            co_return std::move(rep);
         }
-        co_await seastar::sleep(100ms);
+
+        auto uid = req->get_query_param("uid");
+        auto operation = get_operation_type(req->get_query_param("operation"));
+        auto mongo_class = get_mongo_class(req->get_query_param("class"));
+        bsoncxx::document::view_or_value doc = bsoncxx::from_json(req->content);
+
+        LG.info("Received uid: {}", uid);
+        LG.info("Received operation: {}", req->get_query_param("operation"));
+        LG.info("Received class: {}", req->get_query_param("class"));
+        LG.info("Received json: {}", bsoncxx::to_json(doc));
+
+
+        bsoncxx::document::view_or_value result_doc;
+        switch (operation)
+        {
+            case crud_operation::CREATE:
+                {
+                    switch (mongo_class)
+                    {
+                        case mongo_class::db:
+                            {
+                                co_await MongoDBInterface::createDB(doc.view()["name"].get_string());
+                                break;
+                            }
+                        case mongo_class::collection:
+                            {
+                                co_await MongoDBInterface::createCollection(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view().find("collection_options")->get_document().view()
+                                );
+                                break;
+                            }
+                        case mongo_class::document:
+                            {
+                                co_await MongoDBInterface::createDocument(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view()["json_data"].get_document().view()
+                                );
+                                break;
+                            }
+                        case mongo_class::index:
+                            {
+                                co_await MongoDBInterface::createIndex(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view()["keys"].get_document().view(),
+                                    doc.view().find("index_options")->get_document().view()
+                                );
+                                break;
+                            }
+                    }
+                    break;
+                }
+            case crud_operation::READ:
+                {
+                    switch (mongo_class)
+                    {
+                        case mongo_class::db:
+                            {
+                                auto res = co_await MongoDBInterface::readDB(doc.view()["name"].get_string(), doc.view()["list_all"].get_bool());
+                                if (res) { result_doc = *res; }
+                                break;
+                            }
+                        case mongo_class::collection:
+                            {
+                                auto res = co_await MongoDBInterface::readCollection(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view()["list_all"].get_bool()
+                                );
+                                if (res) { result_doc = *res; }
+                                break;
+                            }
+                        case mongo_class::document:
+                            {
+                                auto res = co_await MongoDBInterface::readDocument(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view()["filter"].get_document().view(),
+                                    doc.view()["options"].get_document().view()
+                                );
+                                if (res) { result_doc = *res; }
+                                break;
+                            }
+                        case mongo_class::index:
+                            {
+                                auto res = co_await MongoDBInterface::readIndex(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view()["indexname"].get_string()
+                                );
+                                if (res) { result_doc = *res; }
+                                break;
+                            }
+                    }
+                    break;
+                }
+            case crud_operation::UPDATE:
+                {
+                    switch (mongo_class)
+                    {
+                        case mongo_class::db:
+                            {
+                                throw std::runtime_error("Cannot update a database");
+                            }
+                        case mongo_class::collection:
+                            {
+                                co_await MongoDBInterface::updateCollection(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["oldname"].get_string(),
+                                    doc.view()["newname"].get_string()
+                                );
+                                break;
+                            }
+                        case mongo_class::document:
+                            {
+                                co_await MongoDBInterface::updateDocument(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view()["updmany"].get_bool(),
+                                    doc.view()["filter"].get_document().view(),
+                                    doc.view()["fields_toupd"].get_document().view()
+                                );
+                                break;
+                            }
+                        case mongo_class::index:
+                            {
+                                throw std::runtime_error("Cannot update an index");
+                            }
+                    }
+                    break;
+                }
+            case crud_operation::DELETE:
+                {
+                    switch (mongo_class)
+                    {
+                        case mongo_class::db:
+                            {
+                                co_await MongoDBInterface::deleteDB(doc.view()["name"].get_string());
+                                break;
+                            }
+                        case mongo_class::collection:
+                            {
+                                co_await MongoDBInterface::deleteCollection(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string()
+                                );
+                                break;
+                            }
+                        case mongo_class::document:
+                            {
+                                co_await  MongoDBInterface::deleteDocument(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view()["delmany"].get_bool(),
+                                    doc.view()["filter"].get_document().view()
+                                );
+                                break;
+                            }
+                        case mongo_class::index:
+                            {
+                                co_await MongoDBInterface::deleteIndex(
+                                    doc.view()["dbname"].get_string(),
+                                    doc.view()["collectionname"].get_string(),
+                                    doc.view()["indexname"].get_string()
+                                );
+                                break;
+                            }
+                    }
+                    break;
+                }
+        }
+        rep->_content = bsoncxx::to_json(result_doc);
+        rep->_headers["Content-Type"] = "application/json";
+
+        rep->done("json");
+
+        // co_await seastar::sleep(100ms);
         co_return std::move(rep);
-        // return make_ready_future<std::unique_ptr<http::reply>>(std::move(rep));
+        CATCH
+        {
+            LG_ERROR("Error in crud_handler: {}", std::current_exception());
+            rep->set_status(http::reply::status_type::internal_server_error);
+            rep->done("json");
+            co_return std::move(rep);
+        }
     }
 };
 
@@ -125,102 +335,13 @@ seastar::future<> main_fut()
     seastar::listen_options lo;
     lo.reuse_address = false;
     lo.lba = server_socket::load_balancing_algorithm::round_robin;
-    co_await server->listen(uint16_t(10000), lo);
+    co_await server->listen(uint16_t(6666), lo);
 
     co_await stop_signal.wait();
     co_await server->stop();
     co_await crud_handlers.stop();
     co_return;
 }
-int main(int ac, char** av)
-{
-    std::set_terminate(__gnu_cxx::__verbose_terminate_handler);
-
-    return app.run(ac, av, [&]
-        {
-            return seastar::async([&] ()
-                {
-                    main_fut().get();
-                });
-        });
-}
-
-
-#ifdef DOXYGEN
-seastar::future<> handle_connection(seastar::connected_socket s)
-{
-    auto out = s.output();
-    auto in = s.input();
-    return do_with(std::move(s), std::move(out), std::move(in),
-        [] (auto& s, auto& out, auto& in)
-        {
-            return seastar::repeat([&out, &in]
-                {
-                    return in.read().then([&out] (auto buf)
-                        {
-                            if (buf)
-                            {
-                                LG.info("Received: {}", buf.get());
-                                return out.write(std::move(buf)).then([&out]
-                                    {
-                                        return out.flush();
-                                    }).then([]
-                                        {
-                                            return seastar::stop_iteration::no;
-                                        });
-                            }
-                            else
-                            {
-                                return seastar::make_ready_future<seastar::stop_iteration>(seastar::stop_iteration::yes);
-                            }
-                        });
-                }).then([&out]
-                    {
-                        return out.close();
-                    });
-        });
-}
-
-seastar::future<> service_loop()
-{
-    seastar::listen_options lo;
-    lo.reuse_address = false;
-    return seastar::do_with(seastar::listen(seastar::make_ipv4_address({ 10000 }), lo),
-        [] (auto& listener)
-        {
-            return seastar::keep_doing([&listener] ()
-                {
-                    return listener.accept().then([] (seastar::accept_result res)
-                        {
-                            // Note we ignore, not return, the future returned by
-                            // handle_connection(), so we do not wait for one
-                            // connection to be handled before accepting the next one.
-                            (void) handle_connection(std::move(res.connection)).handle_exception(
-                                [] (std::exception_ptr ep)
-                                {
-                                    fmt::print(stderr, "Could not handle connection: {}\n", ep);
-                                });
-                        });
-                });
-        });
-}
-
-
-
-seastar::future<> main_fut()
-{
-    // seastar_apps_lib::stop_signal stop_signal;
-    LG.set_level(seastar::log_level::trace);
-
-    mongocxx::instance inst{};
-    co_await db_clients.Init();
-
-    co_await seastar::parallel_for_each(boost::irange<unsigned>(0, seastar::smp::count),
-        [] (unsigned c)
-        {
-            return seastar::smp::submit_to(c, service_loop);
-        });
-}
 
 int main(int ac, char** av)
 {
@@ -234,4 +355,3 @@ int main(int ac, char** av)
                 });
         });
 }
-#endif
